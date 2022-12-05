7a267c9250d18b9747135797bacf706d7fc288b4
gregbarajas <gregbarajas96@gmail.com>
2022-12-04 16:40:55 -0800

    Created Registration page and confirmed I can successfully add users.

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("<int:id>", views.index, name="index"),
     path("", views.home, name="name"),
     path("create/", views.create, name="create"),
+    path("register/", views.create, name="create"),
 ]
