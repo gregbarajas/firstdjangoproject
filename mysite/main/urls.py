e7af34761bbd3628a27beeb0cdcd408e72ef46b3
gregbarajas <gregbarajas96@gmail.com>
2022-12-04 17:57:40 -0800

    Added ability for user to add and remove items from a list.

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path("<int:id>", views.index, name="index"),
-    path("", views.home, name="name"),
+    path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path("create/", views.create, name="create"),
-    path("register/", views.create, name="create"),
+    path("view/", views.view, name="view"),
 ]
